82c5eac5fee8fa2125cf5bb0e5fc179436ecfa84
Egoooooooor <71525238+Egoooooooor@users.noreply.github.com>
2020-09-29 16:07:13 +0300

    Add files via upload

---
+++ 201-723 Андреев Егор лабораторная работа №4 задание №2.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------------
+# Name:
+# Purpose:
+#
+# Author:      Егор
+#
+# Created:     29.09.2020
+# Copyright:   (c) Егор 2020
+
+a=int(input())
+b=int(input())
+srednee=(a+b)/2
+print(srednee)
---
+++ 201-723 Андреев Егор лабораторная работа №4 задание №4.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:
+# Purpose:
+#
+# Author:      Егор
+#
+# Created:     29.09.2020
+# Copyright:   (c) Егор 2020
+
+x=int(input())
+y=int(input())
+x3=abs(x)
+y3=abs(y)
+Sum2=x3+y3
+Pro2=x3*y3
+Cha2=x3/y3
+print(Sum2, Pro2, Cha2)
+
+
