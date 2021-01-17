c29f7dd5ed642a276cf41a323f0fa218147db974
Egoooooooor <71525238+Egoooooooor@users.noreply.github.com>
2021-01-17 19:49:45 +0300

    Create 201-723 Андреев Егор лабораторная работа №13 задание №4.c

---
+++ 201-723 Андреев Егор лабораторная работа №13 задание №4.c
@@ -0,0 +1,19 @@
+//13.4
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{   
+    setlocale(LC_ALL, "Rus");  
+    
+    int N,a,n;
+    printf("введите a, N\n");
+    scanf("%d %d",&a,&N);
+        for (int i=0;i<=N;i++)
+            n+=pow(a,i);
+    printf("%d",n);
+        
+    return 0;
+}
